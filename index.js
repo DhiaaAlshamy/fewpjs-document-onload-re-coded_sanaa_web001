1319e3e1c49f71a24d4747ae55bc787d82073d56
Dhiaa Alshamy <dhodhof@gmail.com>
2020-07-23 21:52:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+   console.log("Dh");
+  document.getElementById("text").textContent="This is really cool!";
+  
+   
+  });
+  
